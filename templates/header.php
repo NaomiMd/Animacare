87cdb27e76af0333918939ef71342ad2b6aa8ac8
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-01 16:48:18 +0200

    add footer, header, navbar and navbar style

---
+++ templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bienvenue sur le site de la Clinique Vétérinaire Animacare">
+    <link rel="stylesheet" href="<?= generateLink("assets/bootstrap.min.css") ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="<?= generateLink("assets/style.css") ?>">
+    <title>Clinique Vétérinaire Animacare</title>
+</head>
+<body>
+    
---
+++ templates/navbar.php
@@ -0,0 +1,44 @@
+
+<header>
+<nav class="navbar navbar-expand-lg fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand"><img class="logo" src="<?= generateLink("assets/img/logo.png") ?>" alt="Logo de la clinique"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto px-4">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">La clinique</a>
+        </li>
+        <li class="nav-item pe-4">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+
+      <div class="d-flex px-5 boxing">
+      <div class="box1">
+            <a href="">
+            <div><i id="account" class="bi bi-person-circle"></i></div>
+            <div>Accèder à son compte</div>
+            </a>
+        </div>
+        <div class="box2">
+            <div>Urgence 7j/7</div>
+            <div>24h/24</div>
+            <div><strong>0245133</strong></div>
+        </div>
+        <div class="box3 pe-5">
+          <a href="">
+           <div><i class="bi bi-calendar2-week"></i></div> 
+            <div>Prendre RDV</div>
+        </a>
+        </div>
+     </div>
+    </div>
+  </div>
+</nav>
+</header>
