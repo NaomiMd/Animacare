20e4a5809b42755981ddcd84dfe242eb6e0359d1
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-02 15:34:14 +0200

    add navbar + navbar sytle and header style

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,43 +1,45 @@
-
 <header>
-<nav class="navbar navbar-expand-lg fixed-top">
-  <div class="container-fluid">
+<nav class="navbar fixed-top navbar-expand-lg p-0">
+  <div class="container-fluid navbar_fluid">
     <a class="navbar-brand"><img class="logo" src="<?= generateLink("assets/img/logo.png") ?>" alt="Logo de la clinique"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto px-4">
-        <li class="nav-item">
+        <div class="mx-auto p-0"></div>
+      <ul class="navbar-nav">
+        <li class="nav-item pt-3">
           <a class="nav-link" aria-current="page" href="#">Accueil</a>
         </li>
-        <li class="nav-item">
+       
+        <li class="nav-item pt-3">
           <a class="nav-link" href="#">La clinique</a>
         </li>
-        <li class="nav-item pe-4">
-          <a class="nav-link" href="#">Contact</a>
+        <li class="nav-item pt-3">
+          <a class="nav-link" href="#">
+            <span>Contact</span>
+        </a>
+        </li>
+        <li class="nav-block text-center p-0" id="account" >
+          <a class="nav-link" href="#">
+            <i class="bi bi-person-circle"></i>
+            <span>Accèder au compte</span>
+        </a>
+        </li>
+        <li class="nav-block text-center p-0" id="urgency" >
+          <div class="nav-link">
+            <span>Urgence 24h/7j</span>
+            <span><strong>0245133</strong></span>
+        </div>
+        </li>
+        <li class="nav-block text-center p-0" id="calendar">
+          <a class="nav-link" href="#">
+          <i class="bi bi-calendar2-week"></i>
+            <span>Prendre RDV</span>
+        </a>
         </li>
       </ul>
-
-      <div class="d-flex px-5 boxing">
-      <div class="box1">
-            <a href="">
-            <div><i id="account" class="bi bi-person-circle"></i></div>
-            <div>Accèder à son compte</div>
-            </a>
-        </div>
-        <div class="box2">
-            <div>Urgence 7j/7</div>
-            <div>24h/24</div>
-            <div><strong>0245133</strong></div>
-        </div>
-        <div class="box3 pe-5">
-          <a href="">
-           <div><i class="bi bi-calendar2-week"></i></div> 
-            <div>Prendre RDV</div>
-        </a>
-        </div>
-     </div>
+ 
     </div>
   </div>
 </nav>
