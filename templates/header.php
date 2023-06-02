fc4e0844e8ad231d011f17aa023db911d7cb76b4
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-02 15:33:42 +0200

    add icon boostrapt

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Bienvenue sur le site de la Clinique Vétérinaire Animacare">
-    <link rel="stylesheet" href="<?= generateLink("assets/bootstrap.min.css") ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= generateLink('assets/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="<?= generateLink("assets/style.css") ?>">
     <title>Clinique Vétérinaire Animacare</title>
