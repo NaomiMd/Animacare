530fa01c40f99f7d22585a8d4512c9a9e29e697b
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-07 16:24:24 +0200

    add clinique page with style and image

---
+++ templates/laClinique.php
@@ -0,0 +1,40 @@
+<div class="container-fluid clinique_header">
+    <h1>La Clinique</h1>
+    <h3>Découvrez la clinique et son équipe</h3>
+</div>
+
+<div class="container equipement">
+    <h4>La clinique et son équipement</h4>
+    <p>Notre clinique vétérinaire est équipée d'un matériel de pointe pour fournir des soins vétérinaires de qualité supérieure. <br>
+    Des scanners aux laboratoires d'analyses, en passant par nos salles d'opérations , 
+    nous sommes déterminés à offrir à vos animaux de compagnie les meilleurs traitements possibles.</p>
+</div>
+    <div class="container image_equipement" >
+        <img src="<?= generateLink("assets/img/equipement.jpg") ?>" alt="equipement de la clinique">
+        <img src="<?= generateLink("assets/img/laborantin.jpg") ?>" alt="équipier de la clinique">
+    </div>
+
+
+<div class="container-fluid bloc">
+</div>
+
+<div class="container equipe">
+    <h4>L'équipe</h4>
+    <div class="doctor">
+    <div class="doctor_img">
+        <img src="<?= generateLink("assets/img/team(4).jpg") ?>" class="round d-flex" alt="">
+        <h5>Docteur Name</h5>
+        <h5>Titre</h5>
+    </div>
+    <div class="doctor_img">
+        <img src="<?= generateLink("assets/img/team(4).jpg") ?>" class="round d-flex" alt="">
+        <h5>Docteur Name</h5>
+        <h5>Titre</h5>
+    </div>
+    <div class="doctor_img">
+        <img src="<?= generateLink("assets/img/team(4).jpg") ?>" class="round d-flex" alt="">
+        <h5>Docteur Name</h5>
+        <h5>Titre</h5>
+    </div>
+    </div>
+</div>
