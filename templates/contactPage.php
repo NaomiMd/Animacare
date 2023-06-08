33db4b062e8a62b2395b4c4f99b28448b68b3f39
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-08 11:53:11 +0200

    add contact page and the style

---
+++ templates/contactPage.php
@@ -0,0 +1,32 @@
+<div class="container-fluid contact-header">
+    <h1>Informations contacts</h1>
+</div>
+
+<div class="container informations d-flex">
+    <div class="container consultation">
+        <h4>Consultation <i class="bi bi-calendar-week"></i></h4>
+        <p>Du lundi au samedi de 8h à 20h sur RDV. <br>
+           <em> Pour les urgences merci de nous contacter par téléphone.</em>  
+        </p>
+        <div class="btn btn-consultation">
+            <a href="">prendre rendez-vous</a>
+        </div>
+    </div>
+    <div class="container urgence">
+    <h4>Urgence <i class="bi bi-telephone"></i></h4>
+        <p>Les urgences sont ouvertes 24/7. <br>
+            Dans la mesure du possible, merci de nous appeler au préalable afin que nous préparions votre venue.
+        </p>
+        <span>02 4513 3</span>
+    </div>
+    <div class="container access">
+    <h4>Accès <i class="bi bi-pin-map"></i></h4>
+        <p>Métro :
+            Ligne 14 : station Olympiades <br>
+            Ligne 7 : station Porte d’Ivry <br>
+            Ligne 6 : station Nationale <br>
+            Tramway : Station Porte d’Ivry <br>
+            Bus : Ligne 83, 62 et 64
+        </p>
+    </div>
+</div>
