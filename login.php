b39b3812ee44e9a5ede0cc7072641314b47d66e4
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-08 16:18:43 +0200

    add placeholder

--- login.php
+++ login.php
@@ -9,12 +9,12 @@ require_once _ROOT_ . "/templates/navbar.php";
   <form method="post">
   <div class="mb-3 mt-4">
     <label for="exampleInputEmail1" class="form-label">Adresse email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" class="form-control" placeholder="JDave@exemple.com" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
 
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" placeholder="******" class="form-control" id="exampleInputPassword1">
   </div>
 
   <div class="mb-3 d-flex justify-content-end">
