fcf5f44db1c305952dc7454526b3f6a8e67ec3d0
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-14 17:19:36 +0200

    add style to view schedule page

---
+++ Admin/Schedule/viewSchedule.php
@@ -0,0 +1,28 @@
+<?php
+require_once("../../config.php");
+require_once _ROOT_ . "./templates/header.php";
+require_once _ROOT_ . "/Admin/templates/navbarSide.php";
+?>
+
+<div class="container table-container">
+    <h3 class="text-center mb-5" >Les horaires de la clinique</h3>
+<table class="table  table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Heure d'ouverture</th>
+      <th scope="col">Heure de fermeture</th>
+      <th scope="col">Opération</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td><a href="<?= generateLink("Admin/Schedule/updateSchedule.php")?>" class="btn btn-modify">Modifier</a></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<?php 
+require_once _ROOT_ . "/Admin/templates/footer.php";
+?>
