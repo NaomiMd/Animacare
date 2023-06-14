0295a64e55290f11a5edba839488566842132bbc
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-14 16:35:22 +0200

    add home page for dashboard admin

---
+++ Admin/Dashboard.php
@@ -0,0 +1,13 @@
+<?php
+require_once("../config.php");
+require_once _ROOT_ . "./templates/header.php";
+require_once _ROOT_ . "/Admin/templates/navbarSide.php";
+?>
+
+<div class="container home text-center">
+<h3>Gestion de l'administration de la clinique</h3>
+</div>
+
+<?php 
+require_once _ROOT_ . "/Admin/templates/footer.php";
+?>
