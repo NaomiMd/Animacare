c81514f69fddbd1297321d96745a19a7c0fe7a2a
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-15 17:36:45 +0200

    add gallery view page

---
+++ Admin/Gallery/viewGallery.php
@@ -0,0 +1,34 @@
+<?php
+require_once("../../config.php");
+require_once _ROOT_ . "./templates/header.php";
+require_once _ROOT_ . "/Admin/templates/navbarSide.php";
+?>
+
+<div class="container table-container">
+    <h3 class="text-center mb-5" >La galerie photo</h3>
+<table class="table  table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Nom</th>
+      <th scope="col">Url de l'image</th>
+      <th class="col">Opérations</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>
+        <a href="<?= generateLink("Admin/Gallery/deletePhoto.php")?>" class="btn btn-modify">Supprimer</a>
+    </td>
+    </tr>
+  </tbody>
+</table>
+
+    <div class="text-center mt-5" >
+    <a href="<?= generateLink("Admin/Gallery/addPhoto.php")?>" class="btn btn-modify">Ajouter</a>
+    </div>
+</div>
+<?php 
+require_once _ROOT_ . "/Admin/templates/footer.php";
+?>
