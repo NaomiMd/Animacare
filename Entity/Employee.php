59be83e7668f4a6b712b71595f59642c14f6ef53
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-19 15:27:27 +0200

    add entity for employee

---
+++ Entity/Employee.php
@@ -0,0 +1,128 @@
+<?php
+
+class Employee
+{
+    private $id;
+    private $fname;
+    private $lname;
+    private $title;
+    private $photo;
+
+    public function __construct(array $data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data)
+    {
+        foreach($data as $key => $value)
+        {
+            $method = 'set' .ucfirst($key);
+
+            if(method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of fname
+     */ 
+    public function getFname()
+    {
+        return $this->fname;
+    }
+
+    /**
+     * Set the value of fname
+     *
+     * @return  self
+     */ 
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lname
+     */ 
+    public function getLname()
+    {
+        return $this->lname;
+    }
+
+    /**
+     * Set the value of lname
+     *
+     * @return  self
+     */ 
+    public function setLname($lname)
+    {
+        $this->lname = $lname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of photo
+     */ 
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * Set the value of photo
+     *
+     * @return  self
+     */ 
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
