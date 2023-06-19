1bc21a18fca1a021173d64addfdd3bd243c61ef1
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-19 15:05:21 +0200

    add entity for appointment type

---
+++ Entity/AppointmentType.php
@@ -0,0 +1,65 @@
+<?php 
+
+class AppointmentType
+{
+    private $id;
+    private $name;
+
+    public function __construct(array $data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data)
+    {
+        foreach($data as $key => $value)
+        {
+            $method = 'set' .ucfirst($key);
+
+            if(method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
