2c6b43caccdf913c0823eae261c9f88cd53bd206
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-19 14:56:15 +0200

    add entity for animal

---
+++ Entity/Animal.php
@@ -0,0 +1,151 @@
+<?php
+
+
+class Animal
+{
+    private $id;
+    private $name;
+    private $age;
+    private $sex;
+    private $user;
+    private $species;
+
+
+    public function __constructor(array $data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data)
+    {
+        foreach($data as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+
+            if(method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of age
+     */ 
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set the value of age
+     *
+     * @return  self
+     */ 
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of sex
+     */ 
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    /**
+     * Set the value of sex
+     *
+     * @return  self
+     */ 
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user
+     */ 
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of user
+     *
+     * @return  self
+     */ 
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of species
+     */ 
+    public function getSpecies()
+    {
+        return $this->species;
+    }
+
+    /**
+     * Set the value of species
+     *
+     * @return  self
+     */ 
+    public function setSpecies($species)
+    {
+        $this->species = $species;
+
+        return $this;
+    }
+}
