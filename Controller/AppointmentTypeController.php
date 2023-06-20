8cddccd2f7b037847761fd25fe5e5127af7c87f0
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-20 17:55:50 +0200

    add appointment type controller

---
+++ Controller/AppointmentTypeController.php
@@ -0,0 +1,46 @@
+<?php
+require_once _ROOT_ . '/Entity/AppointmentType.php';
+
+class AppointmentTypeController
+{
+    private $pdo;
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO('mysql:host=mysql;dbname=Animacare;charset=utf8mb4', 'root', 'root');
+        } catch(Exception $e) {
+            echo('Erreur :' .$e->getMessage());
+        }
+    }
+    
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+    public function setPdo(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+        return $this;
+    }
+
+    public function getAll(): array
+    {
+        $appointment_types = [];
+        $req = $this->pdo->query("SELECT * FROM `appointment_type`");
+        $data = $req->fetchAll();
+        foreach($data as $appointment_type)
+        {
+            $appointment_types[] = new AppointmentType($appointment_type);
+        }
+        return $appointment_types;
+    }
+    public function getById($id): AppointmentType
+    {
+        $req = $this->pdo->prepare("SELECT * FROM `appointment-type` WHERE id=:id");
+        $req->bindParam(":id", $id, PDO::PARAM_INT);
+        $req->execute();
+        $data = $req->fetch();
+        $appointment_type = new AppointmentType($data);
+        return $appointment_type;
+    }
+}
