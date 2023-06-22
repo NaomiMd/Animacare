7a6175d3167408f4a30012262f750bde6a112982
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-22 12:21:07 +0200

    add schedule controller and hydrate function

--- Admin/Schedule/updateSchedule.php
+++ Admin/Schedule/updateSchedule.php
@@ -1,7 +1,20 @@
 <?php
 require_once("../../config.php");
-require_once _ROOT_ . "./templates/header.php";
+require_once _ROOT_ . "/templates/header.php";
 require_once _ROOT_ . "/Admin/templates/navbarSide.php";
+require_once _ROOT_ . '/Controller/ScheduleController.php';
+
+
+$scheduleController = new ScheduleController();
+$schedule = $scheduleController->getSchedulebyId(($_GET['id']));
+
+if($_POST)
+{
+    $schedule->hydrate($_POST);
+    $scheduleController->updateSchedule($schedule);
+    header('location: viewSchedule.php');
+}
+
 ?>
 
 <div class="container table-container">
@@ -9,12 +22,12 @@ require_once _ROOT_ . "/Admin/templates/navbarSide.php";
 
     <form method="post" class="schedule-form" >
         <div class="mb-3">
-            <label for="opening_hour" class="form-label">Heure d'ouverture</label>
-            <input type="time" name="opening_hour" class="form-control">
+            <label for="opening_time" class="form-label">Heure d'ouverture</label>
+            <input type="time" name="opening_time" class="form-control" value="<?= $schedule->getOpening_time();?>">
         </div>
         <div class="mb-3">
-            <label for="opening_hour" class="form-label">Heure d'ouverture</label>
-            <input type="time" name="opening_hour" class="form-control">
+            <label for="closing_time" class="form-label">Heure d'ouverture</label>
+            <input type="time" name="closing_time" class="form-control" value="<?= $schedule->getClosing_time();?>">
         </div>
         <div class="mt-5 text-center">
             <button type="submit" class="btn btn-modify">Modifier</button>
