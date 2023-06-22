7daa4e442e723b8dd4da402bf1c492222365c56c
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-22 12:20:20 +0200

    add link id to the update schedule

--- Admin/Schedule/viewSchedule.php
+++ Admin/Schedule/viewSchedule.php
@@ -1,12 +1,18 @@
 <?php
 require_once("../../config.php");
-require_once _ROOT_ . "./templates/header.php";
-require_once _ROOT_ . "/Admin/templates/navbarSide.php";
+require_once _ROOT_ . '/templates/header.php';
+require_once _ROOT_ . '/Admin/templates/navbarSide.php';
+require_once _ROOT_ . '/Controller/ScheduleController.php';
+
+$scheduleController = new ScheduleController();
+$schedules = $scheduleController->getAll();
 ?>
 
 <div class="container table-container">
     <h3 class="text-center mb-5" >Les horaires de la clinique</h3>
     <div class="table-responsive">
+      <?php foreach($schedules as $schedule) : 
+        ?>
 <table class="table table-hover table-bordered">
   <thead>
     <tr>
@@ -17,12 +23,13 @@ require_once _ROOT_ . "/Admin/templates/navbarSide.php";
   </thead>
   <tbody>
     <tr>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td><a href="<?= generateLink("Admin/Schedule/updateSchedule.php")?>" class="btn btn-modify">Modifier</a></td>
+      <td><?= $schedule->getOpening_time(); ?></td>
+      <td><?= $schedule->getClosing_time(); ?></td>
+      <td><a href="updateSchedule.php?id=<?= $schedule->getId();?>" class="btn btn-modify">Modifier</a></td>
     </tr>
   </tbody>
 </table>
+<?php endforeach; ?>
 </div>
 </div>
 <?php 
