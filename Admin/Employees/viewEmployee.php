a49cde8e9ecf15d8e707f05fb5b25b0a40618871
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-23 15:13:42 +0200

    add image tag and link to show the correct photo of each employee

--- Admin/Employees/viewEmployee.php
+++ Admin/Employees/viewEmployee.php
@@ -1,7 +1,11 @@
 <?php
 require_once("../../config.php");
-require_once _ROOT_ . "./templates/header.php";
+require_once _ROOT_ . "/templates/header.php";
 require_once _ROOT_ . "/Admin/templates/navbarSide.php";
+require_once _ROOT_ . "/Controller/EmployeeController.php";
+
+$employeeController = new EmployeeController();
+$employees = $employeeController->getAll();
 ?>
 
 <div class="container table-container">
@@ -18,19 +22,23 @@ require_once _ROOT_ . "/Admin/templates/navbarSide.php";
       <th scope="col">Opération</th>
     </tr>
   </thead>
+  <?php 
+    foreach($employees as $employee) :
+  ?>
   <tbody>
     <tr>
-      <td>Id</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>Véto</td>
-      <td>Photo url</td>
+      <td><?= $employee->getId();?></td>
+      <td><?= $employee->getFname();?></td>
+      <td><?= $employee->getLname();?></td>
+      <td><?= $employee->getTitle();?></td>
+      <td><img alt="Photo de <?= $employee->getFname();?>" src="../../assets/img/<?= $employee->getPhoto();?>" class="d-block mx-lg-auto img-fluid"  width="200" height="500" loading="lazy"></td>
       <td>
-        <a href="<?= generateLink("Admin/Employees/updateEmployee.php")?>" class="btn btn-modify">Modifier</a> 
-       <a href="<?= generateLink("Admin/Employees/deleteEmployee.php")?>" class="btn btn-modify">Supprimer</a>
+        <a href="updateEmployee.php?id=<?= $employee->getId();?>" class="btn btn-modify">Modifier</a> 
+       <a href="deleteEmployee.php?id=<?= $employee->getId();?>?>" class="btn btn-modify">Supprimer</a>
     </td>
     </tr>
   </tbody>
+  <?php endforeach; ?>
 </table>
 <div class="text-center mt-5" >
     <a href="<?= generateLink("Admin/Employees/addEmployee.php")?>" class="btn btn-modify">Ajouter</a>
