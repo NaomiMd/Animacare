918d7ff2b2fae1dc7115f7680970ec8213b27ec2
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-06-23 23:48:03 +0200

    add gallery controller and img src

--- Admin/Gallery/viewGallery.php
+++ Admin/Gallery/viewGallery.php
@@ -1,7 +1,11 @@
 <?php
 require_once("../../config.php");
-require_once _ROOT_ . "./templates/header.php";
+require_once _ROOT_ . "/templates/header.php";
 require_once _ROOT_ . "/Admin/templates/navbarSide.php";
+require_once _ROOT_ . "/Controller/GalleryController.php";
+
+$galleryController = new GalleryController();
+$galleries = $galleryController->getAll();
 ?>
 
 <div class="container table-container">
@@ -11,24 +15,24 @@ require_once _ROOT_ . "/Admin/templates/navbarSide.php";
   <thead>
     <tr>
       <th scope="col">Nom</th>
-      <th scope="col">Url de l'image</th>
       <th scope="col">Image</th>
       <th class="col">Opérations</th>
     </tr>
   </thead>
+  <?php foreach($galleries as $gallery) :?>
   <tbody>
     <tr>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td><img src="" alt=""></td>
+      <td><?= $gallery->getName();?></td>
+      <td><img src="../../assets/img/<?= $gallery->getImage_url(); ?>" alt="<?= $gallery->getName(); ?>"  class="d-block mx-lg-auto img-fluid"  width="200" loading="lazy"></td>
       <td>
-        <a href="<?= generateLink("Admin/Gallery/deletePhoto.php")?>" class="btn btn-modify">Supprimer</a>
+        <a href="deletePhoto.php?id=<?= $gallery->getId(); ?>" class="btn btn-modify">Supprimer</a>
     </td>
     </tr>
   </tbody>
+  <?php endforeach;?>
 </table>
 
-    <div class="text-center mt-5" >
+    <div class="text-center mt-5 mb-5" >
     <a href="<?= generateLink("Admin/Gallery/addPhoto.php")?>" class="btn btn-modify">Ajouter</a>
     </div>
 </div>
