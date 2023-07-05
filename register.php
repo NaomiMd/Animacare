5f56016343871c2dbf36232dde94d2b05d3e2511
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-07-05 15:35:52 +0200

    add register page

--- register.php
+++ register.php
@@ -2,33 +2,66 @@
 require_once("config.php");
 require_once _ROOT_ . "/templates/header.php";
 require_once _ROOT_ . "/templates/navbar.php";
+require_once _ROOT_ . "/Controller/UserController.php";
+
+$userController = new UserController();
+
+if($_POST)
+{
+  $fname = htmlspecialchars($_POST['fname']);
+  $lname = htmlspecialchars($_POST['lname']);
+  $email= htmlspecialchars( $_POST["email"]);
+  $phone= htmlspecialchars( $_POST["phone_number"]);
+  $password = htmlspecialchars($_POST['password']);
+  $confirmedPassword = htmlspecialchars($_POST['confirmedPassword']);
+
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+  {
+    echo '<h4 class="text-center mt-5 pt-5" style="color: #F6B352">Votre email n\'est pas valide</h4>';
+  }elseif($password === $confirmedPassword)
+  {
+    unset($confirmedPassword);
+    $newUser = new User($_POST);
+    $userController->createUser($newUser);
+    $_SESSION['user']['email'] = $_POST['email'];
+    header('location: index.php');
+  }else{
+    echo '<h4 class="text-center mt-5 pt-5" style="color: #F6B352">Vos mots de passe ne correspondent pas</h4>';
+  }
+
+}
 ?>
 
 <div class="container connexion">
     <h4 class="text-center mb-5" >S'inscrire</h4>
   <form method="post">
   <div class="mb-3 mt-4">
-    <label for="fname" class="form-label">Nom</label>
-    <input type="fname" placeholder="Nom" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="fname" class="form-label login">Nom</label>
+    <input type="text" name="fname" placeholder="Nom" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
   </div>  
 
   <div class="mb-3">
-    <label for="lname" class="form-label">Prénom</label>
-    <input type="lname" class="form-control" placeholder="Prénom" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="lname" class="form-label login">Prénom</label>
+    <input type="text" name="lname" class="form-control" placeholder="Prénom" id="exampleInputEmail1" aria-describedby="emailHelp" required>
   </div>  
 
   <div class="mb-3">
-    <label for="email" class="form-label">Adresse email</label>
-    <input type="email" placeholder="JDave@exemple.com" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="email" class="form-label login">Adresse email</label>
+    <input type="email" name="email" placeholder="JDave@exemple.com" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
   </div>
 
   <div class="mb-3">
-    <label for="password" class="form-label">Mot de passe</label>
-    <input type="password" placeholder="*******" class="form-control" id="exampleInputPassword1">
+    <label for="phone_number" class="form-label login">Téléphone</label>
+    <input type="tel" name="phone_number" placeholder="Téléphone" class="form-control" required>
+  </div>
+
+  <div class="mb-3">
+    <label for="password" class="form-label login">Mot de passe</label>
+    <input type="password" name="password" placeholder="*******" class="form-control" id="exampleInputPassword1" required>
   </div>
   <div class="mb-3">
-    <label for="confirmed_password" class="form-label">Confirmez votre mot de passe</label>
-    <input type="confirmed_password"  placeholder="*******"  class="form-control" id="exampleInputPassword1">
+    <label for="confirmedPassword" class="form-label login">Confirmez votre mot de passe</label>
+    <input type="password" name="confirmedPassword"  placeholder="*******"  class="form-control" id="exampleInputPassword1" required>
   </div>
 
     <div class="mt-4 mb-4 d-flex justify-content-center" >
