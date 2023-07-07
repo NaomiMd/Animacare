b72ac9f8bd188bbc79b646f86a0f4c103ea05802
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-07-07 15:58:40 +0200

    add isset session admin to prevent user to have access to the dashboard

--- Admin/Dashboard.php
+++ Admin/Dashboard.php
@@ -1,8 +1,14 @@
 <?php
 require_once("../config.php");
-require_once _ROOT_ . "./templates/header.php";
+require_once _ROOT_ . "/templates/header.php";
 require_once _ROOT_ . "/Admin/templates/navbarSide.php";
+
+if(!isset($_SESSION['admin']))
+{
+    header('location: ../index.php');
+}
 ?>
+<!-- Ajouter la logique $SESSION pour que la page ne soit accessible qu'à un admin -->
 
 <div class="container home text-center">
 <h3>Gestion de l'administration de la clinique</h3>
