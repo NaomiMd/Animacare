c8736e54a25bbd55e73b9a45660482ad6678acca
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-07-07 16:53:18 +0200

    add session_start

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,9 +11,13 @@
     <meta name="description" content="Bienvenue sur le site de la Clinique Vétérinaire Animacare">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="<?= generateLink('assets/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
+    <link rel="stylesheet" href="<?= generateLink('assets/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= generateLink("assets/style.css") ?>">
+    <link rel="stylesheet" href="<?= generateLink("assets/jquery-ui.min.css") ?>">
+    <link rel="stylesheet" href="<?= generateLink("assets/jquery-ui.theme.min.css") ?>">
+    <link rel="stylesheet" href="<?= generateLink("assets/jquery-ui.structure.min.css") ?>">
     <title>Clinique Vétérinaire Animacare</title>
 </head>
 <body>
