b9079b7b27b34874b854d79c636a0bd73b7962bb
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-07-07 15:56:06 +0200

    add isset session to hide login page if already connected

--- login.php
+++ login.php
@@ -4,7 +4,12 @@ require_once _ROOT_ . "/templates/header.php";
 require_once _ROOT_ . "/templates/navbar.php";
 require_once _ROOT_ . "/Controller/UserController.php";
 require_once _ROOT_ . "/Controller/AdminController.php";
+if(isset($_SESSION['user']) || isset($_SESSION['admin']))
+{
+  header('location: index.php');
+}
 
+var_dump(empty($_SESSION['user']));
 $userController = new UserController();
 $adminController = new AdminController();
 
@@ -23,7 +28,7 @@ if($_POST)
   }elseif($admin)
   {
     $_SESSION['admin'] = ['email' => $admin['email']];
-    header('location: index.php');
+    header('location: Admin/Dashboard.php');
   }else{
     echo '<h4 class="text-center mt-5 pt-5" style="color: #F6B352">Identifiants invalides</h4>';
   }
