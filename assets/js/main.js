82af8a861d920818bade444ead095f190fec783c
Naomi Maindret <naomi.maindret@hotmail.fr>
2023-07-14 18:29:04 +0200

    delete space

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+$( function() {
+    $( "#datepicker" ).datepicker({
+        dateFormat: "yy-mm-dd",
+        minDate: 0,
+        maxDate: '01/12/2023',
+        monthNames: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+        dayNamesMin: [ "Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam" ],
+       onSelect: function(a, b){
+           a = new Date();
+            a.setFullYear(b.selectedYear, b.selectedMonth, b.selectedDay);
+          if(a.getDay() == 0)
+           {
+               $(b.input).val(b.lastVal);
+            }
+        },
+});
+
+    $("#timepicker").timepicker({
+        timeFormat:'H:mm',
+        minTime: "8:00",
+        maxTime: "20:00",
+        interval: 30,
+        dynamic: true
+    })
+  } );
+
 let navbar = document.querySelector(".navbar");
 
 window.addEventListener('scroll', () =>
@@ -8,4 +34,6 @@ window.addEventListener('scroll', () =>
     }else{
         navbar.classList.remove('navbar-scrolled');
     }
-})
+});
+
+
